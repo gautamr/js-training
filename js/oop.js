45d5863dc8d2a506698e80324bf8195dea8699fc
gautamr <groy.pi@gmail.com>
2013-08-07 12:11:32 +0530

    added oop

---
+++ js/oop.js
@@ -0,0 +1,45 @@
+// INHERITANCE
+// define the Person Class
+function Person() {
+  console.log('Person constructor');
+}
+
+Person.prototype.walk = function(){
+  console.log('I am walking!');
+};
+
+Person.prototype.sayHello = function(){
+  console.log('hello');
+};
+
+// define the Student class
+function Student() {
+  // Call the parent constructor
+  console.log('Student constructor');
+  Person.call(this);
+}
+
+// inherit Person
+Student.prototype = new Person();
+
+// correct the constructor pointer because it points to Person
+Student.prototype.constructor = Student;
+ 
+// replace the sayHello method
+Student.prototype.sayHello = function(){
+  console.log('hi, I am a student');
+}
+
+// add sayGoodBye method
+Student.prototype.sayGoodBye = function(){
+  console.log('goodBye');
+}
+
+var student1 = new Student();
+student1.sayHello();
+student1.walk();
+student1.sayGoodBye();
+
+// check inheritance
+console.log(student1 instanceof Person); // true 
+console.log(student1 instanceof Student); // true
